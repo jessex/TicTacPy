6ea379a20a4a62f6435836ae849125c1d12e2633
Joshua Essex <essexjosh@gmail.com>
2010-10-13 02:05:35 -0400

    Beginning to add AI functionality for computer player

--- tictactoe.py
+++ tictactoe.py
@@ -1,4 +1,4 @@
-import string, sys
+import string, sys, random
 
 #game vars
 BOARD_HORIZ = "-------------\n"
@@ -6,6 +6,7 @@ BOARD_VERT_EMPTY = "|   |   |   |\n"
 moves = [" ", " ", " ", " ", " ", " ", " ", " ", " "] #chars at each square
 starting_player = "X" #X is default starting player
 active_player = "X"
+comp_player = "N" #defaults as Human v Human so No computer player
 player_one = "X" #player one defaults to X
 player_two = "O" #player two defaults to O
 game_type = 2 #1 -> 1 player (Human v Comp), 2 -> 2 players (Human v Human)
@@ -50,10 +51,10 @@ def draw_stats():
     o_per = '%.2f' % ((o_wins / games_played) * 100)
     d_per = '%.2f' % ((draws / games_played) * 100)
     res = "=====Session Statistics=====\n"
-    res += "Games: %s\n" % games_played
-    res += "X wins: %s , %s%%\n" % (x_wins, x_per)
-    res += "O wins: %s , %s%%\n" % (o_wins, o_per)
-    res += "Draws: %s , %s%%" % (draws, d_per)
+    res += "Games: %s\n" % int(games_played)
+    res += "X wins: %s , %s%%\n" % (int(x_wins), x_per)
+    res += "O wins: %s , %s%%\n" % (int(o_wins), o_per)
+    res += "Draws: %s , %s%%" % (int(draws), d_per)
     print res
     
 #print the board in its current state, takes in a list of the current moves
@@ -160,7 +161,7 @@ def process_turn(move):
         active_player = "X"
 
 #checks board to see if game decided yet, returns winner or "N" for not finished
-def check_victory():
+def process_finish():
     #check all winning combinations
     if moves[0] != " ":
         if moves[0] == moves[1] == moves[2]: #first row
@@ -192,9 +193,11 @@ def check_victory():
             return "N" #found a blank, so unfinished
     return "D" #no blanks but no winner, game is a draw
         
+        
+"""     ***************     GAME FUNCTIONS     ***************     """
     
 #reset game variables for a new match
-def reset_game():
+def game_reset():
     global moves, active_player
     moves = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
     active_player = starting_player
@@ -230,12 +233,177 @@ def game_loop():
                         flag = True
         #have a valid pick at this point
         process_turn(square)
-        game_check = check_victory()
+        game_check = process_finish()
         if game_check == "X" or game_check == "O" or game_check == "D":
             process_result(game_check)
-            reset_game()
+            game_reset()
             draw_main()
             process_main()
+            
+
+"""     ***************     AI FUNCTIONS     ***************     """
+       
+#determines computer's best move using basic situation-action production system
+def comp_move(moves):
+    if comp_player == "X":
+        human_player = "O"
+    else:
+        human_player = "X"
+        
+    if not("X" in moves) and not("O" in moves): #move to center if no moves yet
+        return 4
+    
+    possible = can_finish(comp_player, moves)
+    if possible != -1: #if comp has a winning move, make it
+        return possible
+    possible = can_finish(human_player, moves)
+    if possible != -1: #if human has a winning move, block it
+        return possible
+        
+    possible = can_pin(comp_player, moves)
+    if possible != -1: #if comp can pin, do it
+        return possible
+    possible = can_pin(human_player, moves)
+    if possible != -1: #if human can pin, block it
+        return possible   
+        
+    possible = one_in_set(comp_player, moves)
+    if possible != -1: #if comp can add, do it
+        return possible   
+    possible = one_in_set(human_player, moves)
+    if possible != -1: #if human alone, get next to it
+        return possible  
+    
+    if moves[4] == " ": #if center still unoccupied, occupy it
+        return 4
+    
+    possible = zero_in_set(moves)
+    if possible != -1: #if comp can pioneer, do it
+        return possible   
+        
+    #at this stage, pick a random square which is unoccupied and return it
+    unoccupied = []
+    for i in range(0,9):
+        if moves[i] == " ":
+            unoccupied.append(i)
+    return unoccupied[random.randint(0,len(unoccupied)-1)]
+    
+#determine if winner can win given the board state (-1 if not)
+def can_finish(winner, moves):
+    #all rows
+    for i in range(0,7):
+        if (i % 3) == 0: #first col
+            if player == moves[i] == moves[i+1] and moves[i+2] == " ":
+                return i+2
+            elif player == moves[i+1] == moves[i+2] and moves[i] == " ":
+                return i
+            elif player == moves[i] == moves[i+2] and moves[i+1] == " ":
+                return i+1
+        
+    #all cols
+    for i in range(0,3):
+        if winner == moves[i] == moves[i+3] and moves[i+6] == " ":
+            return i+6
+        if winner == moves[i] == moves[i+6] and moves[i+3] == " ":
+            return i+3
+    for i in range(3,6):
+        if winner == moves[i] == moves[i+3] and moves[i-3] == " ":
+            return i-3
+       
+    #diagonals
+    if winner == moves[0] == moves[4] and moves[8] == " ":
+        return 8
+    if winner == moves[0] == moves[8] and moves[4] == " ":
+        return 4
+    if winner == moves[4] == moves[8] and moves[0] == " ":
+        return 0
+    if winner == moves[2] == moves[4] and moves[6] == " ":
+        return 6
+    if winner == moves[2] == moves[6] and moves[4] == " ":
+        return 4
+    if winner == moves[4] == moves[6] and moves[2] == " ":
+        return 2
+    
+    return -1
+       
+#determine if pinner can pin the corners given the board state (-1 if not)
+def can_pin(pinner, moves):
+    if pinner == moves[0] == moves[8] and moves[2] == " ":
+        return 2
+    if pinner == moves[0] == moves[8] and moves[6] == " ":
+        return 6
+    if pinner == moves[2] == moves[6] and moves[0] == " ":
+        return 0
+    if pinner == moves[2] == moves[6] and moves[8] == " ":
+        return 8
+        
+    return -1
+       
+#determine if row/col/dia has 2 empty squares given board state (-1 if not)
+def one_in_set(player, moves):
+    #all rows
+    for i in range(0,9):
+        if (i % 3) == 0: #first col
+            if player == moves[i] and moves[i+1] == moves[i+2] == " ":
+                return i+1
+        if ((i-1) % 3) == 0: #second col
+            if player == moves[i] and moves[i-1] == moves[i+1] == " ":
+                return i-1
+        if ((i+1) % 3) == 0: #third col
+            if player == moves[i] and moves[i-1] == moves[i-2] == " ":
+                return i-1
+
+    #all cols
+    for i in range(0,3):
+        if player == moves[i] and moves[i+3] == moves[i+6] == " ":
+            return i+3
+    for i in range(3,6):
+        if player == moves[i] and moves[i-3] == moves[i+3] == " ":
+            return i-3
+    for i in range(6,9):
+        if player == moves[i] and moves[i-6] == moves[i-3] == " ":
+            return i-3
+    
+    #diagonals
+    if player == moves[0] and moves[4] == moves[8] == " ":
+        return 4
+    if player == moves[4] and moves[0] == moves[8] == " ":
+        return 0
+    if player == moves[8] and moves[0] == moves[4] == " ":
+        return 4
+    if player == moves[2] and moves[4] == moves[6] == " ":
+        return 4
+    if player == moves[4] and moves[0] == moves[6] == " ":
+        return 0
+    if player == moves[6] and moves[0] == moves[4] == " ":
+        return 4
+       
+    return -1
+    
+#determine if row/col/dia has 3 empty squares given board state (-1 if not)
+def zero_in_set(moves):
+    #all rows
+    for i in range(0,7):
+        if (i % 3) == 0: #first col
+            if moves[i] == moves[i+1] == moves[i+2] == " ":
+                return i
+                
+    #all cols
+    for i in range(0,3):
+        if moves[i] == moves[i+3] == moves[i+6] == " ":
+            return i
+            
+    #diagonals
+    if moves[0] == moves[4] == moves[8] == " ":
+        return i
+    if moves[2] == moves[4] == moves[6] == " ":
+        return i
+    
+    return -1
+       
+       
+       
+       
        
        
 if __name__ == "__main__":
